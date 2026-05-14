e9a8c14ada3742777da2ee871da5e4c7ae611b17
sagarstar1 <sagarpapta00@gmail.com>
2026-05-14 14:59:24 +0530

    fix cors

--- doorwash-backend/doorwash-backend/server.js
+++ doorwash-backend/doorwash-backend/server.js
@@ -38,10 +38,9 @@ app.set('io', io); // Available in req.app.get('io') in controllers
 
 // Middleware
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://doorwash9-51c6l7t4f-sagarpapta00-8610s-projects.vercel.app"
-  ],
+  origin: function(origin, callback) {
+    callback(null, true);
+  },
   credentials: true,
 }));
 app.use(express.json());
